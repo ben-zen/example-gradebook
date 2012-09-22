2d466741d2a31fab05339b61e6020cd0e17c4802
ben-zen <benjf5+github@gmail.com>
2012-09-21 22:11:37 -0400

    Added Grade.java, in its initial state.

---
+++ Grade.java
@@ -0,0 +1,24 @@
+/* Copyright (C) 2012 Benjamin Lewis
+ * 
+ * Licensed under the GNU GPLv3
+ */
+
+/* Grade.java, an example grading program. */
+
+/* Just some code to start from; we'll see how this can become far more
+ * interesting.
+ */
+
+public class Grade {
+    public static void main (String[] args) {
+	int midtermGrade, finalGrade;
+	midtermGrade = 60;
+	finalGrade = 70;
+	courseGrade = (midtermGrade + finalGrade) / 200;
+	if (courseGrade >= 50) {
+	    System.out.println("Satisfactory");
+	} else {
+	    System.out.println("Unsatisfactory");
+	}
+    }
+}
